68b216d392721a1825469f228ea670c80e5dbf7e
Huy Phan <huy.phan2210@outlook.com>
2023-04-11 17:56:48 +0700

    Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer>
+            <div id='challenger'>
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+                Coded by <a href="https://github.com/huyphan2210" target="_blank">Huy Phan</a>.
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
