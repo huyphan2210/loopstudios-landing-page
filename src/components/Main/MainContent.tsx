48b753b10f87de2927983111000c832ad8f1af2d
Huy Phan <huy.phan2210@outlook.com>
2023-04-11 17:57:22 +0700

    Hero section

---
+++ src/components/Main/MainContent.tsx
@@ -0,0 +1,16 @@
+import './Main.css'
+
+function Main() {
+    return (
+        <main>
+            <section id='hero'>
+                <hgroup>
+                    <h1>Immersive Experiences that deliver</h1>
+                </hgroup>
+            </section>
+        </main>
+    )
+}
+
+
+export default Main;
