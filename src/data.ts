d641c321937c8671e18df145bd9e859fe6e1b9cf
Huy Phan <huy.phan2210@outlook.com>
2023-04-12 17:56:33 +0700

    Create data.ts

---
+++ src/data.ts
@@ -0,0 +1,35 @@
+// Desktop imgs
+import deepEarth from './assets/images/desktop/image-deep-earth.jpg';
+import nightArcade from './assets/images/desktop/image-night-arcade.jpg';
+import soccerTeamVR from './assets/images/desktop/image-soccer-team.jpg';
+import theGrid from './assets/images/desktop/image-grid.jpg';
+import fromUpAboveVR from './assets/images/desktop/image-from-above.jpg';
+import pocketBorealis from './assets/images/desktop/image-pocket-borealis.jpg';
+import theCuriosity from './assets/images/desktop/image-curiosity.jpg';
+import makeItFishEye from './assets/images/desktop/image-fisheye.jpg';
+
+// Mobile imgs
+import deepEarthMobile from './assets/images/mobile/image-deep-earth.jpg';
+import nightArcadeMobile  from './assets/images/mobile/image-night-arcade.jpg';
+import soccerTeamVRMobile  from './assets/images/mobile/image-soccer-team.jpg';
+import theGridMobile  from './assets/images/mobile/image-grid.jpg';
+import fromUpAboveVRMobile  from './assets/images/mobile/image-from-above.jpg';
+import pocketBorealisMobile  from './assets/images/mobile/image-pocket-borealis.jpg';
+import theCuriosityMobile  from './assets/images/mobile/image-curiosity.jpg';
+import makeItFishEyeMobile  from './assets/images/mobile/image-fisheye.jpg';
+
+
+const creations = [
+    { name: 'Deep Earth', imgDesktop: deepEarth, imgMobile: deepEarthMobile }, 
+    { name: 'Night Arcade', imgDesktop: nightArcade, imgMobile: nightArcadeMobile }, 
+    { name: 'Soccer Team VR', imgDesktop: soccerTeamVR, imgMobile: soccerTeamVRMobile }, 
+    { name: 'The Grid', imgDesktop: theGrid, imgMobile: theGridMobile }, 
+    { name: 'From Up Above VR', imgDesktop: fromUpAboveVR, imgMobile: fromUpAboveVRMobile }, 
+    { name: 'Pocket Borealis', imgDesktop: pocketBorealis, imgMobile: pocketBorealisMobile }, 
+    { name: 'The Curiosity', imgDesktop: theCuriosity, imgMobile: theCuriosityMobile }, 
+    { name: 'Make it Fisheye', imgDesktop: makeItFishEye, imgMobile: makeItFishEyeMobile }, 
+]
+
+export default creations;
+
+
