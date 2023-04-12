ad4538c7324c4036189b28b9aa8b8121c8db5a50
Huy Phan <huy.phan2210@outlook.com>
2023-04-12 13:47:40 +0700

    Intro section

--- src/components/Main/MainContent.tsx
+++ src/components/Main/MainContent.tsx
@@ -1,4 +1,10 @@
-import './Main.css'
+import './Main.css';
+
+// Desktop imgs
+import interactive from '../../assets/images/desktop/image-interactive.jpg';
+
+// Mobile imgs
+import interactiveMobile from '../../assets/images/mobile/image-interactive.jpg';
 
 function Main() {
     return (
@@ -7,6 +13,21 @@ function Main() {
                 <hgroup>
                     <h1>Immersive Experiences that deliver</h1>
                 </hgroup>
+            </section>
+            <section id='content'>
+                <section id='intro'>
+                    <img src={interactive} alt="Interactive" loading='lazy'></img>
+                    <img src={interactiveMobile} alt="Interactive" loading='lazy'></img>
+                    <hgroup>
+                        <h1>The leader in interactive vr</h1>
+                        <p>
+                            Founded in 2011, Loopstudios has been producing world-class virtual reality 
+                            projects for some of the best companies around the globe. Our award-winning 
+                            creations have transformed businesses through digital experiences that bind 
+                            to their brand.
+                        </p>
+                    </hgroup>
+                </section>
             </section>
         </main>
     )
